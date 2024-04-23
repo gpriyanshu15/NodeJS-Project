6ee7a198c73ef5a25258e0dd46dd040fc7e5569a
Priyanshu Gupta <92108888+gpriyanshu15@users.noreply.github.com>
2024-04-23 16:49:25 +0530

    new

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ app
   .get((req, res) => {
     const id = Number(req.params.id);
     const user = users.find((user) => user.id === id);
+    if(!user) return res.status(404).json({error:"user not found"});  
     return res.json(user);
   })
   .patch((req, res) => {
@@ -71,7 +72,16 @@ app
 
 app.post("/api/users", (req, res) => {
   const body = req.body;
-  users.push({ ...body, id: users.length + 1 });
+  if (
+    !body ||
+    !body.first_name ||
+    !body.last_name ||
+    !body.email ||
+    !body.gender ||
+    !body.job_title
+  )
+  return res.status(400).json({msg :"All fields are required"});
+    users.push({ ...body, id: users.length + 1 });
   fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
     return res.status(201).json({ status: "SUCCESS", id: users.length });
   });
